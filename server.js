61414a6fd19db18d193ba4dcd28142d2da4ad097
Zaklina Radivojevic <83774205+zaklinaradivojevic@users.noreply.github.com>
2024-01-27 10:47:26 +0100

    Update server.js

--- server.js
+++ server.js
@@ -17,6 +17,12 @@ app.use(cors({origin: '*'})); //For FCC testing purposes only
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Set Content Security Policy headers
+app.use((req, res, next) => {
+  res.setHeader('Content-Security-Policy', "default-src 'self'; script-src 'self'; style-src 'self'");
+  next();
+});
+
 //Index page (static HTML)
 app.route('/')
   .get(function (req, res) {
